f6a741b840cfbd8e11571f2c3b0cc5e1fdf48bc6
Santiago <you@example.com>
2020-05-17 21:16:24 -0300

    bd-creada-y-conectada

--- App.py
+++ App.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_mysqldb import MySQL
 
 app=Flask(__name__)
+
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_PASSWORD"] = "localhost"
+app.config["MYSQL_USER"] = "localhost"
+app.config["MYSQL_DB"] = "pruebaflask" #le pido que se conecte a la base de datos prueba flask
+
+mysql = MySQL(app)
+
+#en templates guardo todo lo que se ve
 
 @app.route("/")
 def index():
